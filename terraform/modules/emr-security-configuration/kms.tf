294fb0d68f9e754ff844ba02ee06d8d8e90454ea
jimbarker <36623939+jimbarker@users.noreply.github.com>
2020-05-18 14:55:06 +0100

    fix key rotation policy for kms keys (#52)
    * fix key rotation policy for kms keys
    
    * add key alias
    
    * update snyk exceptions

--- terraform/modules/emr-security-configuration/kms.tf
+++ terraform/modules/emr-security-configuration/kms.tf
@@ -1,6 +1,7 @@
 resource "aws_kms_key" "emr_sec_config" {
   description             = "${var.name}_key_for_ebs_encryption"
   deletion_window_in_days = 7
+  enable_key_rotation     = true
 
   tags = merge({ "Name" = var.name }, var.common_tags)
 }
--- terraform/modules/fargate-service/kms.tf
+++ terraform/modules/fargate-service/kms.tf
@@ -1,6 +1,16 @@
 resource "aws_kms_key" "encryption_key" {
-  description         = "${var.name_prefix} ECS Encryption Key"
-  is_enabled          = true
-  enable_key_rotation = true
-  tags                = merge(var.common_tags, { Name = "${var.name_prefix}-kms-key" })
+  description             = "${var.name_prefix} ECS Encryption Key"
+  is_enabled              = true
+  enable_key_rotation     = true
+  deletion_window_in_days = 7
+  tags                    = merge(var.common_tags, { Name = "${var.name_prefix}-kms-key" })
+}
+
+resource "aws_kms_alias" "encryption_key" {
+  name          = "alias/emr-cluster-broker/ecs_encryption_key"
+  target_key_id = aws_kms_key.encryption_key.id
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
